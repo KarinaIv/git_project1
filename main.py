8bad8e88a526692191b9f2ff72efe8975ef6d56e
Карина <karinaivanova2005@inbox.ru>
2022-01-10 22:00:15 +0300

    just draw background

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import sys
 
 size = 1000, 700
 screen = pygame.display.set_mode(size)
+surf = pygame.Surface((30, 80))
+surf.fill('#ff9a8d')
 
 running = True
 
@@ -11,5 +13,7 @@ while running:
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
+    screen.fill('#aed6dc')
+    screen.blit(surf, (500, 350))
     pygame.display.update()
 
