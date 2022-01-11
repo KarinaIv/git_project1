1844eff2eb93d3ab2828e1c71c083f175e61e57e
Карина <karinaivanova2005@inbox.ru>
2022-01-11 22:54:00 +0300

    create first block of minecraft snake

--- main.py
+++ main.py
@@ -1,19 +1,30 @@
 import pygame
+from pygame.locals import *
 import sys
 
-size = 1000, 700
-screen = pygame.display.set_mode(size)
-surf = pygame.Surface((30, 80))
-surf.fill('#ff9a8d')
 
-running = True
-
-while running:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            sys.exit()
+if __name__ == '__main__':
+    pygame.init()
+    size = width, height = 1000, 700
+    screen = pygame.display.set_mode(size)
+    pygame.display.set_caption('Snake Game')
     screen.fill('#aed6dc')
-    screen.blit(surf, (500, 350))
-    pygame.display.update()
 
+    block = pygame.image.load('block.png').convert()
+    screen.blit(block, (500, 350))
+
+    pygame.display.flip()
+    running = True
+
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.KEYDOWN:
+                if event.key == K_ESCAPE:
+                    running = False
+            if event.type == pygame.QUIT:
+                running = False
+
+
+        pygame.display.flip()
+
+    pygame.quit()
