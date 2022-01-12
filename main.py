ac6822276abfa3939974103bdf656b5a08231651
Карина <karinaivanova2005@inbox.ru>
2022-01-12 21:57:50 +0300

    теперь тело змейки может перемещаться

--- main.py
+++ main.py
@@ -3,15 +3,25 @@ from pygame.locals import *
 import sys
 
 
+def draw_snake_block():
+    screen.fill('#aed6dc')
+    screen.blit(block, (block_x, block_y))
+    pygame.display.flip()
+
+
 if __name__ == '__main__':
     pygame.init()
+
     size = width, height = 1000, 700
     screen = pygame.display.set_mode(size)
     pygame.display.set_caption('Snake Game')
     screen.fill('#aed6dc')
 
     block = pygame.image.load('block.png').convert()
-    screen.blit(block, (500, 350))
+
+    block_x = 0
+    block_y = 0
+    screen.blit(block, (block_x, block_y))
 
     pygame.display.flip()
     running = True
@@ -21,6 +31,22 @@ if __name__ == '__main__':
             if event.type == pygame.KEYDOWN:
                 if event.key == K_ESCAPE:
                     running = False
+
+                if event.key == K_RIGHT or event.key == K_d:
+                    block_x += 5
+                    draw_snake_block()
+                if event.key == K_LEFT or event.key == K_a:
+                    block_x -= 5
+                    draw_snake_block()
+
+                if event.key == K_UP or event.key == K_w:
+                    block_y -= 5
+                    draw_snake_block()
+                if event.key == K_DOWN or event.key == K_s:
+                    block_y += 5
+                    draw_snake_block()
+
+
             if event.type == pygame.QUIT:
                 running = False
 
